8b65be02e97572168b3f4e82b1fb0aef33d521cd
joeyphang <joeyphangx@gmail.com>
2018-08-12 00:07:56 +0800

    Add Figaro

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
 	has_secure_password
+ 	validates :email, presence: { message: "Email must be filled."}, format: { with: /\w+@\w+\.\w{2,}/, message: "Email must be in format abc@example.com"}
 end
